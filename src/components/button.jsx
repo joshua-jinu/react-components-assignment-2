7c816c04cf0a7cbaf78e83a2bee1c21facfe1c4e
Joshua Jinu <116093853+joshua-jinu@users.noreply.github.com>
2025-01-20 14:39:50 +0530

    added pricecard and button component

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,16 @@
-// write button card here
+import React from 'react'
+
+function button({text, onClick}) {
+    const btnStyle = {
+        backgroundColor: 'blue',
+        padding: '1rem',
+        borderRadius: '10px'
+    }
+    return (
+    <button style={btnStyle} onClick={onClick}>
+        {text}
+    </button>
+  )
+}
+
+export default button
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,24 @@
-// write product card here
+import React from 'react';  
+import Button from './button'; 
+import './productcard.css';  
+
+const Productcard = () => { 
+    const data = { 
+        productImage: 'https://res.cloudinary.com/dabeupfqq/image/upload/v1734699696/uploads/hvvyd3op416lbdrqwxzg.jpg', 
+        productName: 'DJI Ozmo', 
+        price: '19,999'
+    }
+    
+  return (  
+    <div className="productcard">  
+      <img src={data.productImage} alt={data.productName} className="productcard-image" />  
+      <div className="productcard-details">  
+        <h3 className="productcard-name">{data.productName}</h3>  
+        <p className="productcard-price">${data.price}</p>  
+        <Button text="View Product" onClick={() => alert(`Viewing ${data.productName}`)} />  
+      </div>  
+    </div>  
+  );  
+};  
+
+export default Productcard;  
